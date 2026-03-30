a9ead4e8b273ff7cb7ca0e523f0db657aed900d6
Sunny <sunnychalam@Mac.lan>
2026-03-29 20:57:47 -0700

    Adding a simple calculator

---
+++ calculator.py
@@ -0,0 +1,11 @@
+num_1 = float(input('What is your first number? '))
+num_2 = float(input('What is your second number? '))
+
+print(f"{num1} + {num2} = {num1 + num2}")
+print(f"{num1} - {num2} = {num1 - num2}")
+print(f"{num1} * {num2} = {num1 * num2}")
+
+if num2 != 0:
+    print(f"{num1} / {num2} = {num1 / num2}")
+else:
+    print("Can't divide by zero!")
